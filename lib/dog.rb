ce916d4088fa08adb273f728539c6a8af033981c
Kelin Field <kfield111@hotmail.com>
2020-08-11 23:08:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  def name=(dog_name)
+    @this_dog = dog_name
+  end
+
+  def name
+    @this_dog
+  end
+
+  def bark=(bark_sound)
+    @this_bark_sound = bark_sound
+  end
+
+  def bark
+    puts "woof!"
+
+end
